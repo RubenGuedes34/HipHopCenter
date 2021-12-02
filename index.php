248bc962efbbd6d5b25323dba1fb5795561aa4d9
diferreira2000 <diogoferreira299@gmail.com>
2021-12-02 01:26:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 
 
 <?php
-include("includes/configs.php");
-session_destroy();
-if(isset($_SESSION['userLoggedIn'])){
-	$userLoggedIn = $_SESSION ['userLoggedIn'];
-}else{
-	header("Location: register.php");
-}
+
 ?>
 <html>
 <head>
