ecb7d08da98ef6d73c5b7fba7e4cf75ba19e1b4a
tomasoliveira21 <tomasoliveira1018@gmail.com>
2021-12-08 02:28:30 +0000

    1.7 (alpha)
    tou me a passar com O CSS FILHO

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
@@ -94,11 +96,11 @@
                 <div class="col-md-4 col-sm-6">
                     <div class="table">
                         <h3 class="title">Standard</h3>
-                        <div class="">
+                        <div class="price">
                             10.00 paus
-                            <span>/Mês</span>
+                            <span class="month">/Mês</span>
                         </div>
-                        <ul>
+                        <ul class="content">
                             <li>Tomás</li>
                             <li>Tomás</li>
                             <li>Tomás</li>
