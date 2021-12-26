d21916cf7f27770943092f54303ee25c77d50962
tomasoliveira21 <tomasoliveira1018@gmail.com>
2021-12-26 02:42:45 +0000

    Error 404 Not Found

--- resources/views/albuns/show.blade.php
+++ resources/views/albuns/show.blade.php
@@ -30,54 +30,67 @@ use App\Models\Artist;
      <div id="mainContainer">
 
 
-        <div id="topContainer">
+     <div id="topContainer">
 
-                <div id="navBarContainer">
-                    <nav class="navBar">
+            <div id="navBarContainer">
+                <nav class="navBar">
 
-                        <div class="group">
-                            <div class="navItem">
-                                <a href="search.php" class="navItemLink">Search
-                                <img src="{{ url('images/search.png') }}" class="icon" alt="Search">
-                                </a>
-                            </div>
+                <div class="group">
 
-                        </div>
-
-                        <div class="group">
-                            <div class="navItem">
-                                <a href="browse.php" class="navItemLink">Browse</a>
-                            </div>
-
-                            <div class="navItem">
-                                <a href="yourMusic.php" class="navItemLink">Your Music</a>
-                            </div>
-
-                            <div class="navItem">
-                                <a href="profile.php" class="navItemLink">{{ auth()->user()->name }}</a>
-                            </div>
-
-                            <div class="navItem">
+                    <div class="navItem">
+                            <a href="../homepage" class="logo">
+                                <img src="{{url('images/HipHopCenter.gif')}}" alt="Logo">
+                            </a>
                             
-                                <a class="logout"  href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                            document.getElementById('logout-form').submit();">
-                                                {{ __('Logout') }}
-                                </a>
+                    </div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">                                      
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
+                    <div class="navItem">
+                            <a href="search.php" class="navItemLink">
+                            <i class="fas fa-search"></i>
+                            Search</a>
+                        </div>
 
+                        <div class="navItem">  
+                            <a href="../homepage" class="navItemLink">
+                                <i class="fas fa-check"> </i>
+                                Recomendações</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">
+                                <i class="fas fa-music"></i>
+                                Your Music</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">
+                                <i class="fas fa-user"></i>
+                                {{ auth()->user()->name }}</a>
+                        </div>
+
+                        <div class="navItem">
+                        
+                            <a class="logout"  href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                                        <i class="fas fa-sign-out-alt"></i>
+                                            {{ __('Logout') }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">                                      
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                             </div>
 
                         </div>
 
-                    </nav>
-                </div>
+                    </div>
+
+                </nav>
+            </div>
         </div>
+
         <div id="mainViewContainer">
             <div id="mainContent">          
                 <div class="entityInfo">
