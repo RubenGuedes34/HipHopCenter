6385652bac435e4efeba7f338273603fa413d97d
root <root@ip-172-31-47-64.us-east-2.compute.internal>
2022-01-12 15:04:31 +0000

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ app/Http/Middleware/IsPremium.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsPremium
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+       if(auth()->user()->type==1 || auth()->user()->type==2){
+           return $next($request);
+       }
+       return redirect('pagamentos')->with('error',"You dont have Premium access.");
+    }
+}
