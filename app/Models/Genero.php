55356ed4bd0b40d61ba7fbd6833259e6bdcf8acd
diferreira2000 <diogoferreira299@gmail.com>
2022-01-12 14:50:51 +0000

    Genero imortal
    jamais seras apagado

---
+++ app/Models/Genero.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Genero extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nome',
+    
+    ];
+
+
+
+
+    public function musicas()
+    {
+        return $this->hasMany(Musica::class, 'id_genero', 'id');
+    }
+}
