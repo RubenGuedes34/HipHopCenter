bd7dce2e77733e6f21bace4ccbe9f3c837a1149d
tomasoliveira21 <tomasoliveira1018@gmail.com>
2022-01-15 12:59:08 +0000

    erros parte 2
    fixing

--- resources/views/playlists/play.blade.php
+++ resources/views/playlists/play.blade.php
@@ -10,6 +10,8 @@ use App\Models\Artist;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
