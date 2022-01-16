8b94874d47a43ddeda7fb43a5aaf5b593717fcfa
tomasoliveira21 <tomasoliveira1018@gmail.com>
2022-01-16 01:52:36 +0000

    seeders 3.0

--- database/seeders/AlbunsSeeder.php
+++ database/seeders/AlbunsSeeder.php
@@ -13,6 +13,90 @@ class AlbunsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $albums = [
+            [
+                'id' => 1,
+                'nome' => 'Illmatic',
+                'Likes'=> 33,
+                'id_artista' => 1,
+                'capa' => 'illmatic.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'nome' => 'Kanye West Best Songs',
+                'Likes'=> 89,
+                'id_artista' => 2,
+                'capa' => 'kanye.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Maria Leal Hit Songs',
+                'Likes'=> 1,
+                'id_artista' => 3,
+                'capa' => 'maria.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'nome' => '20 anos de Tony Carreira',
+                'Likes'=> 3333,
+                'id_artista' => 4,
+                'capa' => 'tony.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'nome' => 'Rock Xutos',
+                'Likes'=> 89,
+                'id_artista' => 5,
+                'capa' => 'xutos.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 6,
+                'nome' => 'Real Slim Shady',
+                'Likes'=> 89,
+                'id_artista' => 6,
+                'capa' => 'ddd.png',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 7,
+                'nome' => 'Rip 2pac',
+                'Likes'=> 333,
+                'id_artista' => 7,
+                'capa' => '2pac.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 8,
+                'nome' => 'The Notorious',
+                'Likes'=> 89,
+                'id_artista' => 8,
+                'capa' => 'big.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 9,
+                'nome' => 'Enter the Wu-Tang Clan',
+                'Likes'=> 500000,
+                'id_artista' => 9,
+                'capa' => 'enterthewutang.jpg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+        ];
+        Album::insert($albums);
     }
 }
--- database/seeders/ArtistsSeeder.php
+++ database/seeders/ArtistsSeeder.php
@@ -13,6 +13,81 @@ class ArtistsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $artists = [
+            [
+                'id' => 1,
+                'nome' => 'Nas',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 332,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'nome' => 'Kanye West',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 3233,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Maria Leal',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 4,
+                'nome' => 'Tony Carreira',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 89,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 5,
+                'nome' => 'Xutos & Pontapés',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 89,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 6,
+                'nome' => 'Eminem',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 33333,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 7,
+                'nome' => '2pac',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 500000,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 8,
+                'nome' => 'B.I.G',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 333,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 9,
+                'nome' => 'Wu-Tang Clan',
+                'Data_de_nascimento' => NULL,
+                'Likes'=> 89,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+        ];
+        Artist::insert($artists);
     }
 }
