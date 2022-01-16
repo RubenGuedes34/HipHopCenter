1e5eced1d828dcb436ace74cf0dfcf8fa0f88717
tomasoliveira21 <tomasoliveira1018@gmail.com>
2022-01-16 01:14:24 +0000

    seeders 1.0

---
+++ database/seeders/PlaylistsSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PlaylistsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        $playlists = [
+            [
+                'id' => 1,
+                'nome' => 'Quem ta triste nao ta mais',
+                'Likes'=> 500,
+                'id_user' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'nome' => 'Musicas para Alternas',
+                'Likes'=> 500,
+                'id_user' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'nome' => 'Seferovic Playlist',
+                'Likes'=> 500,
+                'id_user' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+
+        ];
+        Playlist::insert($playlists);
+
+
+    }
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        $users = [
+            [
+                'name' => 'Tomás',
+                'email' => 'tomasoliveira1018@gmail.com',
+                'id_lastAlbum' => NULL,
+                'id_lastPlaylist' => NULL,
+                'email_verified_at' => now(),
+                'type' => 2,
+                'password' =>  Hash::make('12345678'),
+                'remember_token' => NULL,
+                'created_at' => now(),
+                'updated_at' => now(),
+                
+            ],
+            [
+                'name' => 'Diogo',
+                'email' => 'diogo@gmail.com',
+                'id_lastAlbum' => NULL,
+                'id_lastPlaylist' => NULL,
+                'email_verified_at' => now(),
+                'type' => 2,
+                'password' =>  Hash::make('12345678'),
+                'remember_token' => NULL,
+                'created_at' => now(),
+                'updated_at' => now(),
+                
+            ],
+            [
+                'name' => 'Ruben',
+                'email' => 'ruben@gmail.com',
+                'id_lastAlbum' => NULL,
+                'id_lastPlaylist' => NULL,
+                'email_verified_at' => now(),
+                'type' => 0,
+                'password' =>  Hash::make('12345678'),
+                'remember_token' => NULL,
+                'created_at' => now(),
+                'updated_at' => now(),
+                
+            ]
+
+        ];
+        User::insert($users);
+
+        
+    }
+}
