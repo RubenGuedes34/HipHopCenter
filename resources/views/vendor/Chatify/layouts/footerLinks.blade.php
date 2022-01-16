5945b483ba3e37ab347ddfa223adbecad3368909
rubenguedes39917 <rubenguedes34@gmail.com>
2022-01-16 20:35:59 +0000

    chat upgrade

---
+++ resources/views/vendor/Chatify/layouts/footerLinks.blade.php
@@ -0,0 +1,19 @@
+<script src="https://js.pusher.com/7.0.3/pusher.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script >
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  var pusher = new Pusher("{{ config('chatify.pusher.key') }}", {
+    encrypted: true,
+    cluster: "{{ config('chatify.pusher.options.cluster') }}",
+    authEndpoint: '{{route("pusher.auth")}}',
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+        }
+    }
+  });
+</script>
+<script src="{{ asset('js/chatify/code.js') }}"></script>
