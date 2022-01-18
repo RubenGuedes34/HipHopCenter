d5662d158c6ff7cb9cde96c02cd75827e098fbad
tomasoliveira21 <tomasoliveira1018@gmail.com>
2022-01-18 15:46:05 +0000

    boas praticas mvc

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,19 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="{{url('css/homepage.css')}}">
-    <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/61e35012f7cf527e84d256df/1fpfv1ifs';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+
 
     <title>Hip-hop Center</title>
 </head>
@@ -175,6 +163,21 @@ s0.parentNode.insertBefore(s1,s0);
         }
     </script>
     
+
+        <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61e35012f7cf527e84d256df/1fpfv1ifs';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 
 </html>
